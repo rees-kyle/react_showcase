067a8c577b758dc6e1cdb5bdc1a0c043a3b9b3cf
rees-kyle <rees-kyle@outlook.com>
2024-12-18 04:19:52 +0000

    Modify EmbeddingExpressions component
    - Removed image.
    - Removed css file.
    - Added code element.

--- project/src/components/basics/jsxsyntax/EmbeddingExpressions.js
+++ project/src/components/basics/jsxsyntax/EmbeddingExpressions.js
@@ -1,6 +1,3 @@
-import embeddingExpressions from './../../../assets/embedding_expressions.png';
-import './embedding-expressions.css';
-
 const EmbeddingExpressions = () => {
     const name = "Kyle";
     return (
@@ -9,9 +6,11 @@ const EmbeddingExpressions = () => {
             <p>You can embed any valid JavaScript expression inside curly braces in JSX. For example, variables or function calls can be included within JSX.</p>
 
             <h3>Code:</h3>
-            <div className="img-container">
-                <img src={embeddingExpressions} alt="Embedding Expresisons" />
-            </div>
+            <pre>
+                <code>
+                    {`const name = "Kyle";\n<h1 className="output">Hello, {name}!</h1>;`}
+                </code>
+            </pre>
 
             <h3>Output:</h3>
             <h1 className="output">Hello, {name}!</h1>
