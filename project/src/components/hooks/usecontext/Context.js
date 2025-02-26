dff0ee2d0c31a1f201f7aed109b453c148702b80
rees-kyle <rees-kyle@outlook.com>
2025-02-26 18:23:44 +0000

    Add Context component

---
+++ project/src/components/hooks/usecontext/Context.js
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+// Create a Context object to hold the global state
+// Optional: You can provide a default value in the parentheses (null in this case)
+const Context = createContext(null);
+
+export default Context;
