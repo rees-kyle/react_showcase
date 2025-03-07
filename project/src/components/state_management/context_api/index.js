2f479f481b2688f4f4aef2124de403db74a12116
rees-kyle <rees-kyle@outlook.com>
2025-03-07 02:17:19 +0000

    Add KeyPoints component
    - Added component file.
    - Added to index in components folder.
    - Imported and added component to page.

--- project/src/components/state_management/context_api/index.js
+++ project/src/components/state_management/context_api/index.js
@@ -2,3 +2,4 @@ export { default as ContextAPI } from './ContextAPI';
 export { default as How } from './How';
 export { ThemeProvider, ThemeContext } from './ThemeProvider'; 
 export { default as ThemedComponent } from './ThemedComponent';
+export {default as KeyPoints} from './KeyPoints';
