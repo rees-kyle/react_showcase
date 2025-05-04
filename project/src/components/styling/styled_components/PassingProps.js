9ac6055b5898cd045b91af653c4a6f155b0d3694
rees-kyle <rees-kyle@outlook.com>
2025-05-05 00:44:32 +0100

    Modify PassingProps component
    - Added transient prop to avoid React DOM warning and ESLint error.
    - Added 'type' attribute to prevent unexpected form submissions.
    - Modified code examples with the new code.

--- project/src/components/styling/styled_components/PassingProps.js
+++ project/src/components/styling/styled_components/PassingProps.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import styled from 'styled-components';
 
+// Transient prop ($primary) is used so it doesn't get passed to the DOM
 const Button = styled.button`
-  /* Set background color based on the 'primary' prop */
-  background-color: ${props => props.primary ? '#E07A5F' : '#008CBA'};
+  /* Set background color based on the '$primary' prop */
+  background-color: ${props => props.$primary ? '#E07A5F' : '#008CBA'};
   color: white;
   padding: 0.5rem 1rem;
   border: none;
@@ -10,22 +12,22 @@ const Button = styled.button`
   text-align: center;
   text-decoration: none;
   display: inline-block;
-  border-radius: 0.3rem;
   font-size: 1.2rem;
   margin: 0rem 1rem;
   margin-bottom: 2rem;
   cursor: pointer;
-  /* Change background color on hover, based on the 'primary' prop */
+
+  /* Change background color on hover, based on the '$primary' prop */
   &:hover {
-    background-color: ${props => props.primary ? '#B75C43' : '#005f73'};
+    background-color: ${props => props.$primary ? '#B75C43' : '#005f73'};
   }
 `;
 
 const PassingProps = () => {
   return (
     <div>
-      <br/>
-      <hr/>
+      <br />
+      <hr />
       <h2>Passing Props to Styled Components</h2>
       <p>
         You can also make your styled components dynamic by passing props. For instance, changing the button color based on a 'primary' prop.
@@ -34,10 +36,11 @@ const PassingProps = () => {
       <h3>Code:</h3>
       <pre>
         <code>
-          {`import styled from 'styled-components';
+{`import React from 'react';
+import styled from 'styled-components';
 
 const Button = styled.button\`
-  background-color: \${props => props.primary ? '#E07A5F' : '#008CBA'};
+  background-color: \${props => props.$primary ? '#E07A5F' : '#008CBA'};
   color: white;
   padding: 0.5rem 1rem;
   border: none;
@@ -45,47 +48,44 @@ const Button = styled.button\`
   text-align: center;
   text-decoration: none;
   display: inline-block;
-  border-radius: 0.3rem;
   font-size: 1.2rem;
   margin: 0rem 1rem;
   margin-bottom: 2rem;
   cursor: pointer;
 
   &:hover {
-    background-color: \${props => props.primary ? '#B75C43' : '#005f73'};
+    background-color: \${props => props.$primary ? '#B75C43' : '#005f73'};
   }
-\`;
-`}
+\`;`}
         </code>
       </pre>
 
       <p>
-        Now, pass the 'primary' prop to control the button’s style.
+        Now, pass the '$primary' prop to control the button’s style.
       </p>
 
       <pre>
         <code>
-          {`const PassingProps = () => {
+{`const PassingProps = () => {
   return (
     <div>
-      <Button primary>Primary Button</Button>
+      <Button $primary type="button">Primary Button</Button>
       <br/>
-      <Button>Default Button</Button>
+      <Button type="button">Default Button</Button>
     </div>
   );
 };
 
-export default PassingProps;
-`}
+export default PassingProps;`}
         </code>
       </pre>
 
       <h3>Output:</h3>
-      {/* Render a Button component with the 'primary' prop applied, making it a "Primary Button" */}
-      <Button primary>Primary Button</Button>
-      <br/>
-      {/* Render a Button component without the 'primary' prop, making it a "Default Button" */}
-      <Button>Default Button</Button>
+      {/* Render a Button component with the '$primary' prop applied, making it a "Primary Button" */}
+      <Button $primary type="button">Primary Button</Button>
+      <br />
+      {/* Render a Button component without the '$primary' prop, making it a "Default Button" */}
+      <Button type="button">Default Button</Button>
     </div>
   );
 };
