3369e6376ccdcc00abb71c569ef2dc7b93db8ac5
rees-kyle <rees-kyle@outlook.com>
2025-05-06 22:36:32 +0100

    Add React StrictMode to index.js

--- project/src/index.js
+++ project/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -10,13 +10,13 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-
+  <StrictMode>
     <Provider store={store}>
       <ProviderComponent>
         <App />
       </ProviderComponent>
     </Provider>
-
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
